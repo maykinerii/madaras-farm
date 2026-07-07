c54a56f230fe4f139ab4308c19c839abc8a141ad
Mayki Neri <143084803+maykinerii@users.noreply.github.com>
2026-07-07 06:26:39 +0000

    container image size

--- src/components/activities.jsx
+++ src/components/activities.jsx
@@ -61,6 +61,7 @@ export default function Activities({ lightboxImg, openLightbox, closeLightbox })
     }
   };
 
+
   return (
     <section id="activities" className="activities">
       {/* Combined Showcase Section */}
@@ -75,11 +76,10 @@ export default function Activities({ lightboxImg, openLightbox, closeLightbox })
             <Slider ref={sliderRef} {...sliderSettings}>
               {allImages.map((image, idx) => (
                 <div key={idx} className="carousel-slide">
-                  <div className="image-card">
+                  <div className="image-card" onClick={() => openLightbox(image.src)}>
                     <img 
                       src={image.src} 
                       alt={image.alt} 
-                      onClick={() => openLightbox(image.src)}
                       className="carousel-image"
                     />
                     <div className="image-overlay">
